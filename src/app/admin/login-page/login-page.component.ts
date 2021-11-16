65a4e61def4313153995c4fd107f304398ab48f5
Zhenya <epol1394@gmail.com>
2021-11-16 22:21:27 +0300

    first change

---
+++ src/app/admin/login-page/login-page.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.scss']
+})
+export class LoginPageComponent implements OnInit {
+
+  public form: FormGroup;
+
+  constructor() {
+    this.form = new FormGroup({
+      email: new FormControl(null, [
+        Validators.required,
+        Validators.email
+      ]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6)
+      ])
+    })
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  submit() {
+    if (this.form.invalid) {
+      return
+    }
+  }
+
+}
