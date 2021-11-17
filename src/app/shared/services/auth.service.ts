ec264d78ab8b09682033df731a33aa17ff4cbcea
Zhenya <epol1394@gmail.com>
2021-11-17 22:32:09 +0300

    started authService

---
+++ src/app/shared/services/auth.service.ts
@@ -0,0 +1,35 @@
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {User} from "../interfaces";
+import {Observable} from "rxjs";
+import {environment} from "../../../environments/environment";
+import {tap} from "rxjs/operators";
+import {FbAuthResponse} from "../../../environments/interface";
+
+@Injectable()
+export class AuthService {
+constructor(private http: HttpClient) {}
+
+  get token(): string {
+    return ''
+}
+
+login(user: User): Observable<any> {
+  return this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`, user)
+    .pipe(
+      tap(this.setToken)
+    )
+}
+
+logout() {
+
+}
+
+isAuthenticated(): boolean {
+  return !!this.token
+}
+
+private setToken(response: any) {
+  console.log(response)
+}
+}
